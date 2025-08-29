57bb40a542427058442b2c00eb603bf41756db5f
shPallavi <singhhhpallavi@gmail.com>
2025-08-29 14:05:23 +0530

    Added hello.js with math function testing

--- hello.js
+++ hello.js
@@ -1,1 +1,5 @@
-console.log("Hello world");
+const math = require("crypto");
+// const { addFun, subFun } = require("./index");
+
+// console.log("Maths Value is : ", addFun(3, 4), subFun(2, 1));
+console.log("add function", math);
